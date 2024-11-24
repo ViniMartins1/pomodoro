61ebb25ab38c74fcd808286723886ce0b03f4848
Vinícius E. A. Martins <viniciusmartins308@gmail.com>
2024-11-24 15:12:19 -0300

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PomodoroApp extends StatelessWidget {
+  const PomodoroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pomodoro app',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/app.dart';
+
+void main() => runApp(const PomodoroApp());
